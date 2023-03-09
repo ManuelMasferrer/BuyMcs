3b1b2e67c4934afef04f3fec44b96049fd15a77c
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2023-03-09 17:50:55 -0600

    Âupdated to publish message

--- view/purchase_projection_view.py
+++ view/purchase_projection_view.py
@@ -6,6 +6,11 @@ from flask import request
 from datetime import date, datetime, timedelta
 from sqlalchemy import func
 
+import asyncio
+from azure.servicebus.aio import ServiceBusClient
+from azure.servicebus import ServiceBusMessage
+from azure.identity.aio import DefaultAzureCredential
+
 # from azure.storage.queue import (
 #         QueueClient,
 #         BinaryBase64EncodePolicy,
@@ -14,9 +19,32 @@ from sqlalchemy import func
 
 import os, uuid
 
+NAMESPACE_CONNECTION_STR = "Endpoint=sb://sbpurchases.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=TAZI8x82mRdU4/QXYQ8XyWXhUlov26467+ASbLGDaoE="
+QUEUE_NAME = "buyqueue"
+
+
+async def send_single_message(sender, message):
+        # Create a Service Bus message and send it to the queue
+        # message = ServiceBusMessage("Single Message")
+    await sender.send_messages(message)
+
+
+async def run(message):
+    # create a Service Bus client using the connection string
+    async with ServiceBusClient.from_connection_string(
+        conn_str=NAMESPACE_CONNECTION_STR,
+        logging_enable=True) as servicebus_client:
+            # Get a Queue Sender object to send messages to the queue
+        sender = servicebus_client.get_queue_sender(queue_name=QUEUE_NAME)
+        async with sender:
+                # Send one message
+            await send_single_message(sender, message)
+
 class ViewPurchaseProjection(Resource):
 
 
     def get(self, product_name):
+        message = ServiceBusMessage(product_name)
+        asyncio.run(run(message))
 
-        return jsonify("Proyeccion de ventas solicitada")
+        return jsonify("Proyeccion de compras solicitada")
