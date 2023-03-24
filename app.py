f6b915259c59b6c689ab70967bfb4292e5a07edf
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2023-03-24 02:11:45 -0600

    add http exceptions

--- app.py
+++ app.py
@@ -17,5 +17,24 @@ api.add_resource(ViewPurchaseProjection, '/api/projection/purchases/<string:prod
 def home():
     return Response("{'a':'b'}", status=201, mimetype='application/json')
 
+@app.errorhandler(NotFound)
+def page_not_found_handler(e: HTTPException):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(Unauthorized)
+def unauthorized_handler(e: HTTPException):
+    return render_template('401.html'), 401
+
+
+@app.errorhandler(Forbidden)
+def forbidden_handler(e: HTTPException):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(RequestTimeout)
+def request_timeout_handler(e: HTTPException):
+    return render_template('408.html'), 408
+
 if __name__ == "__main__":
     app.run(port = 5000, debug = True, host='0.0.0.0')
