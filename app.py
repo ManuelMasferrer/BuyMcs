d12805b182e5895218f8f0fd51c7aa0e8550af02
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2023-03-24 02:42:08 -0600

    error handlers

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_marshmallow import Marshmallow
 from flask import Response
 from view.purchase_projection_view import ViewPurchaseProjection
+from werkzeug.exceptions import Forbidden, HTTPException, NotFound, RequestTimeout, Unauthorized
 
 app = Flask(__name__)
 
@@ -16,6 +17,25 @@ api.add_resource(ViewPurchaseProjection, '/api/projection/purchases/<string:prod
 @app.route('/')
 def home():
     return Response("{'a':'b'}", status=201, mimetype='application/json')
+
+@app.errorhandler(NotFound)
+def page_not_found_handler(e: HTTPException):
+    return "404"
+
+
+@app.errorhandler(Unauthorized)
+def unauthorized_handler(e: HTTPException):
+    return "401"
+
+
+@app.errorhandler(Forbidden)
+def forbidden_handler(e: HTTPException):
+    return "403"
+
+
+@app.errorhandler(RequestTimeout)
+def request_timeout_handler(e: HTTPException):
+    return "408"
 
 if __name__ == "__main__":
     app.run(port = 5000, debug = True, host='0.0.0.0')
